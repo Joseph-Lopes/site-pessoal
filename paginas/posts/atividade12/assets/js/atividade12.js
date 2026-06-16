4c0e4390ede3f51d80fb7b241ece99eefc1b28cf
José Lopes <joselopes.psr@gmail.com>
2026-06-15 21:48:30 -0300

    correções visuais e adições na pág. contato

--- paginas/posts/atividade12/assets/js/atividade12.js
+++ paginas/posts/atividade12/assets/js/atividade12.js
@@ -1,20 +1,25 @@
 function calcularMedia() {
-    let situacao;
-
     let nome = document.getElementById("nome").value;
-    let primeiraNota = document.getElementById("primeiraNota").value;
-    let segundaNota = document.getElementById("segundaNota").value;
-    let terceiraNota = document.getElementById("terceiraNota").value;
+    let nota1 = parseFloat(document.getElementById("nota1").value);
+    let nota2 = parseFloat(document.getElementById("nota2").value);
+    let nota3 = parseFloat(document.getElementById("nota3").value);
 
-    let media = (primeiraNota + segundaNota + terceiraNota) / 3;
+    let media = (nota1 + nota2 + nota3)/3;
+
+    let classe;
 
     if (media >= 7) {
-        situacao = "aprovado";
-    } else if (media > 5 && media < 7) {
-        situacao = "recuperação";
-    } else if (media < 5) {
-        situacao = "reprovado";
+        resultado = `Nome: ${nome}; <br>Situação: aprovado; <br>Média: ${media.toFixed(2)};`;
+        classe = "aprovado";
+    } else if (media >= 4 && media <= 6.9) {
+        resultado = `Nome: ${nome}; <br>Situação: recuperação; <br>Média: ${media.toFixed(2)};`
+        classe = "recuperacao";
+    } else {
+        resultado = `Nome: ${nome}; <br>Situação: reprovado; <br>Média: ${media.toFixed(2)};`
+        classe = "reprovado";
     }
 
-    
+    let div = document.getElementById("resultado");
+    div.innerHTML = resultado;
+    div.className = classe;
 }
